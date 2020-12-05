3255d20987420e04f603d1b12ccde8f52c212fc2
MooYeol Prescott Lee <mooyoul@gmail.com>
2020-12-05 18:26:38 +0900

    Update configuration

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { LookupResult, Target } from "./target";
 // tslint:disable:max-line-length
 const SLACK_REPORT_ENABLED = new Set([1, 9]).has(new Date().getHours()); // KST 10AM OR 6PM
 const Targets: Target[] = [
-  new Target("https://www.cosstores.com/en_de/men/menswear/knitwear/jumpers/product.knitted-cotton-merino-jumper-blue.0911266001.html"),
+  new Target("https://www.cosstores.com/en_de/men/menswear/knitwear/jumpers/product.knitted-cotton-merino-jumper-blue.0911266001.html", ["S", "M"]),
   new Target("https://www.cosstores.com/en_de/men/accessories/shoes/boots/product.chunky-sole-chelsea-boots-black.0784114002.html", ["0784114002005"]),
   new Target("https://www.cosstores.com/en_usd/women/womenswear/coats-and-jackets/coats/product.long-hooded-puffer-coat-yellow.0916508002.html"),
 ];
